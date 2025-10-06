48a53edbf8d5cbbbfd9ef61fa4cc0cda8a6a1dc6
Jogo1001 <101247726@georgebrown.ca>
2025-10-06 17:19:51 -0400

    player abilities added

--- Assets/Scripts/EncounterManager.cs
+++ Assets/Scripts/EncounterManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -5,13 +6,34 @@ using UnityEngine.UI;
 public class EncounterManager : MonoBehaviour
 {
 
+    [Header("Characters")]
+    public Character player;
+    public Character enemy;
 
+    [Header("UI Elements")]
+    public Button fleeButton;
+    public Transform abilityButtonContainer;
+    public Button abilityButtonPrefab;
+
+    private List<Ability> playerAbilities = new List<Ability>();
+    private bool playerTurn = true;
 
     private void Start()
     {
-
+        InitializeAbilities();
     }
+    private void InitializeAbilities()
+    {
+   
+        bool firstWin = PlayerPrefs.GetInt("FirstWin", 0) == 1;
 
+        playerAbilities.Clear();
+        playerAbilities.Add(new Ability("Tackle", 10, "A basic hit."));
+        playerAbilities.Add(new Ability("Blast", 15, "A stronger attack."));
+
+        if (firstWin)
+            playerAbilities.Add(new Ability("Lightning", 20, "A powerful new move!"));
+    }
     public void Flee()
     {
         Debug.Log("Player fled the battle!");
