8b3cd8c7c8acd972db6f183e655667eff4fc7ba9
Jogo1001 <101247726@georgebrown.ca>
2025-11-03 13:03:33 -0500

    item collider test

--- Assets/Scripts/ItemPickup.cs
+++ Assets/Scripts/ItemPickup.cs
@@ -1,9 +1,23 @@
 using UnityEngine;
 using TMPro;
+using System.Security.Cryptography.X509Certificates;
 
 public class ItemPickup : MonoBehaviour
 {
-   
+    [Header("Item")]
+     public string itemName;
+
+    [TextArea]
+    public string description;
+
+    [Header("UI")]
+    public GameObject pickupUI;
+    public GameObject itemMessageUI;
+    public TextMeshProUGUI itemMessageText;
+
+
+
+
     void Start()
     {
         
@@ -13,5 +27,20 @@ public class ItemPickup : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("Test");
+        }
+    }
+   void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Debug.Log("exit");
+        }
     }
 }
